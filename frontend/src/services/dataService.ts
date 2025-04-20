c948cf62d3f2ee1c244ba7103d4a4915f5357b09
Jim Lu <jimlo7760@gmail.com>
2025-04-19 22:48:42 -0400

    create FetchData from dataService

---
+++ frontend/src/services/dataService.ts
@@ -0,0 +1,20 @@
+import GetApiServerUri from 'components/helpers';
+import IsManager from 'components/is_manager';
+import axios from 'axios';
+import apiEndpoints from 'components/apiConfig';
+
+
+
+const fetchData = async (endpoint: string, isManager: boolean, serverSelected?: string) => {
+    let url = GetApiServerUri(endpoint);
+    if (isManager && serverSelected) {
+        url += `/${serverSelected}`;
+    }
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
+};
